5dc2a0676a07c53d0af989d227bef4d19adaa252
ThenmozhiLawrence <thenmozhipradeep@gmail.com>
2022-02-07 22:00:57 -0600

    build run'

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -7,7 +7,7 @@ class Navbar extends Component {
          <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
              <div class="container-fluid">
                 <i class="fas fa-university fa-3x"></i> 
-                <a class="navbar-brand" class="font-weight-bolder" class="link-dark" href="https://www.google.com">
+                <a class="navbar-brand font-weight-bolder link-dark" href="https://www.google.com">
                 <h2>Bad Bank</h2></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
