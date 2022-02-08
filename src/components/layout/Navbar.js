735d9df4986184e45b5229475606a127bfac5dd1
ThenmozhiLawrence <thenmozhipradeep@gmail.com>
2022-02-07 21:53:36 -0600

    changed href

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -7,15 +7,15 @@ class Navbar extends Component {
          <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
              <div class="container-fluid">
                 <i class="fas fa-university fa-3x"></i> 
-                <a class="navbar-brand" class="font-weight-bolder" class="link-dark" href="#">
+                <a class="navbar-brand" class="font-weight-bolder" class="link-dark" href="https://www.google.com">
                 <h2>Bad Bank</h2></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                   <div class="navbar-nav">
-                    <a class="nav-link active" aria-current="page" href="#"><b>Home</b></a>
-                    <a class="nav-link" href="#"> <b>Contact Us</b></a>
+                    <a class="nav-link active" aria-current="page" href="https://www.google.com"><b>Home</b></a>
+                    <a class="nav-link" href="https://www.google.com"> <b>Contact Us</b></a>
                   </div>
                 </div>
             </div>
